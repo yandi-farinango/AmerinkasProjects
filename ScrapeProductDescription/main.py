6ea718268b30050da65fdbf84a6ecb1a4b22a897
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2022-08-29 16:35:43 -0500

    Updated Scrape

--- ScrapeProductDescription/main.py
+++ ScrapeProductDescription/main.py
@@ -1,12 +1,18 @@
 import docx
+import pprint
 
 doc = docx.Document('Stones.docx')
+
+products = set()
 
 for table in doc.tables:
     for row in table.rows:
         for cell in row.cells:
-            if cell.paragraphs[1]:
-                print(cell.paragraphs[1].text)
+            if len(cell.paragraphs) > 1:
+                products.add(cell.paragraphs[1].text.strip())
             else:
                 continue
 
+
+products.remove("")
+pprint.pprint(products)
